7779d5ffa6c4be99c9534411819ba45b4b4c8378
miczh111 <mikezust@gmail.com>
2023-11-19 15:48:01 +0100

    = 1.0.22 = * Update: Tested up to WordPress 6.4 * Update: UIKIT CSS & JS to 3.17.10

--- config/theme/theme.php
+++ config/theme/theme.php
@@ -26,7 +26,7 @@ class theme {
 
     $this->displayname = __( 'ZMT Corporate', 'zmt-corporate' );
 
-    $this->version = '1.0.21';
+    $this->version = '1.0.22';
 
     $this->framework = 'zm-uikit';
 
